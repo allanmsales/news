0d78fd913bf70933b1a64e21d5ae427cbdd0c62e
allan sales <allan.sales@bigdatacorp.com.br>
2022-11-05 11:11:19 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,64 @@
+import urllib3
+from bs4 import BeautifulSoup
+
+http = urllib3.PoolManager()
+
+def head_line(url, soup_function):
+    page = http.request('GET', url)
+    soup = BeautifulSoup(page.data, 'html.parser') 
+    return eval(soup_function)
+
+class Portal():
+    def __init__(self, name, url, soup_function):
+        self.name = name
+        self.url = url
+        self.soup_function = soup_function
+
+    def head_line(self):
+        page = http.request('GET', self.url)
+        soup = BeautifulSoup(page.data, 'html.parser') 
+        return eval(self.soup_function)
+
+    def print(self):
+        print(self.name + ': ', self.head_line())
+
+### UOL ###
+uol = Portal('UOL', 'https://www.uol.com.br', 'soup.find("h3", {"class": "title__element"}).text')
+uol.print()
+
+### R7 ###
+r7 = Portal('R7', 'https://www.r7.com/', 'soup.find("a", {"class": "r7-flex-title-h3__link"}).text')
+r7.print()
+
+### VALOR ###
+valor = Portal('VALOR', 'https://valor.globo.com/', 'soup.find("a", {"class": "bstn-dedupe-url"}).text')
+valor.print()
+
+### INFOMONEY ###
+infomoney = Portal('INFOMONEY', 'https://www.infomoney.com.br/', 'soup.find("a", {"class": "cover-link"})["title"]')
+infomoney.print()
+
+### FOLHA DE SP ###
+folha_de_sp = Portal('FOLHA DE SP', 'https://www.folha.uol.com.br/', 'soup.find("h2", {"class": "c-main-headline__title"}).text')
+folha_de_sp.print()
+
+### JOVEM PAN ###
+jovem_pan = Portal('JOVEM PAN', 'https://jovempan.com.br/', 'soup.find("h2", {"class": "title"}).text')
+jovem_pan.print()
+
+### DIÁRIO DO NORDESTE ###
+diario_do_nordeste = Portal('DIÁRIO DO NORDESTE', 'https://diariodonordeste.verdesmares.com.br/', 'soup.find("h2", {"class": "m-c-teaser__heading"}).text')
+diario_do_nordeste.print()
+
+
+### ESTADO DE SP ### BLOQUED
+'''
+valor = Portal('ESTADO DE SP', 'https://www.estadao.com.br/', 'page.data')
+valor.print()
+'''
+
+### G1 ### HASHED
+'''
+valor = Portal('G1', 'https://g1.globo.com/', 'page.data')
+valor.print()
+'''
