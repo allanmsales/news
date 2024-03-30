256feefc27e3df8b24a84dcb931cd15326bbba4a
allan <allanmsales@gmail.com>
2024-03-30 11:30:15 -0300

    refactor: Splitting working crawlers from main class

---
+++ entity/infomoney.py
@@ -0,0 +1,10 @@
+from entity.portal import Portal
+
+
+class Infomoney(Portal):
+    def __init__(self):
+        self.name = 'INFOMONEY'
+        self.url = 'https://www.infomoney.com.br/'
+        self.soup_function = 'soup.find("a", {"class": "cover-link"})["title"]'
+        super()
+        self.print()
