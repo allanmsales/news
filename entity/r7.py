6648d80262300a7aa6fa92318ae20a7a9597f688
allan <allanmsales@gmail.com>
2024-04-05 07:29:33 -0300

    refactor: Spliting The New York Times, The Washigton Post and France 24

--- entity/r7.py
+++ entity/r7.py
@@ -7,4 +7,8 @@ class R7(Portal):
         self.url = 'https://www.r7.com/'
         self.soup_function = 'soup.find("a", {"class": "r7-flex-title-h3__link"}).text'
         super()
-        self.print()
+        try:
+            self.print()
+        except:
+            self.soup_function = 'soup.find("a", {"class": "r7-flex-title-h1__link"}).text'
+            self.print()
---
+++ entity/the_new_york_times.py
@@ -0,0 +1,10 @@
+from entity.portal import Portal
+
+
+class TheNewYorkTimes(Portal):
+    def __init__(self):
+        self.name = 'THE NEW YORK TIMES'
+        self.url = 'https://www.nytimes.com/'
+        self.soup_function = 'soup.find("p", {"class": "indicate-hover"}).text'
+        super()
+        self.print()
