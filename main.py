dd72b68bd3d59ccb03ab66f50dbe0a5831386942
allan <allanmsales@gmail.com>
2024-04-10 08:51:08 -0300

    feature: Adding FastAPI

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+from fastapi import FastAPI
+from server.gunicorn_server import GunicornServer
 from controller import news
+
+app = FastAPI(
+    title="Mews Web Crawler",
+    version="0.1",
+    description="API to get news from web."
+)
+
+app.include_router(news.router)
+
+if __name__ == '__main__':
+    options = {
+        'bind': '{}:{}'.format('0.0.0.0', '8080'),
+        'workers': 1,
+        'worker_class': 'uvicorn.workers.UvicornWorker',
+        'timeout': 600
+    }
+
+    GunicornServer(app, options).run()
